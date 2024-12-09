1fa3abb8389fe64e201d33cdde6473a36852b140
Sampreet Patil <sampreetpatil270@gmail.com>
2024-12-09 22:53:11 +0530

    msg.py update, realization

--- msg.py
+++ msg.py
@@ -1,2 +1,3 @@
-def send_to_client():
+# Tried Twilio and Whatsapp Business API, there are paid though...., I don't know whether we should spend money on this pro yet?
+def send_to_client(): 
     return
