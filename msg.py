7ef81e4319af6f16d8778c9dfaccd868684545ac
Sampreet Patil <sampreetpatil270@gmail.com>
2025-03-11 19:36:33 +0530

    face_detection dataset dir updated, package info added.

--- msg.py
+++ msg.py
@@ -1,3 +1,5 @@
-# Tried Twilio and Whatsapp Business API, there are paid though...., I don't know whether we should spend money on this pro yet?
+# Tried Twilio and Whatsapp Business API, there are paid though...., 
+# I don't know whether we should spend money on this pro yet?
+
 def send_to_client(): 
     return
